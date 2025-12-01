d6eb2e48c67ed4eb39cd13eb89c8bf907bcfd6ed
superstan777 <viir4d@gmail.com>
2025-12-01 11:42:02 +0100

    feat: implement next/image, update next config

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import Image from "next/image";
 import { HomeButton } from "@/components/HomeButton";
 import { SignInWithMicrosoftButton } from "@/components/SignInButton";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
+      {/* Lewa kolumna */}
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center items-center gap-2 md:justify-start">
           <HomeButton withText={true} />
@@ -13,12 +17,16 @@ export default function LoginPage() {
         </div>
       </div>
 
-      <div className=" relative hidden lg:flex items-center justify-center">
-        <div className="w-lg h-lg overflow-hidden rounded-t-full">
-          <img
+      {/* Prawa kolumna */}
+      <div className="relative hidden lg:flex items-center justify-center">
+        <div className="relative w-full max-w-md aspect-square overflow-hidden rounded-t-full">
+          <Image
             src="https://group.vattenfall.com/cdn-cgi/image/width=1494,format=auto,fit=crop,height=1494/globalassets/com/sustainability/beach_sun_1x1.jpg"
             alt="Image"
-            className="w-full h-full object-cover"
+            fill
+            className="object-cover"
+            loading="eager"
+            sizes="(max-width: 1024px) 100vw, 50vw"
           />
         </div>
       </div>
